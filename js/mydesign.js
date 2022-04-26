96fac3cb7026426fedbee794b32da43866142e59
yanhuoi <1421923068@qq.com>
2022-04-26 17:02:49 +0800

    Site updated: 2022-04-26 17:02:38

--- js/mydesign.js
+++ js/mydesign.js
@@ -1,4 +1,5 @@
 let backimg =["url(/img/banner1/0.jpg)","url(/img/banner1/1.jpg)","url(/img/banner1/2.jpg)","url(/img/banner1/3.jpg)","url(/img/banner1/4.jpg)","url(/img/banner1/5.jpg)",,"url(/img/banner1/6.jpg)"];
-let index =Math.ceil(Math.random() * (backimg.length-1))-1;
-// document.getElementById("web_bg").style.backgroundImage = backimg[index]
+// let index =Math.ceil(Math.random() * (backimg.length-1))-1;//随机切换banner图
+// document.getElementById("web_bg").style.backgroundImage = backimg[index]    //随机切换背景
+let index =new Date().getDay();
 document.getElementById("page-header").style.backgroundImage = backimg[index]
