40969a95d349ebb846086dd51ad5c06fa1062afc
yanhuoi <1421923068@qq.com>
2023-03-08 16:42:52 +0800

    Site updated: 2023-03-08 16:42:43

--- zhheo/random.js
+++ zhheo/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/37376.html","posts/57701.html","posts/51393.html","posts/fcb8725d.html","posts/60d9c4db.html","posts/24844.html","posts/20763.html","posts/27281.html","posts/e8e90389.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/37376.html","posts/57701.html","posts/51393.html","posts/24844.html","posts/27281.html","posts/fcb8725d.html","posts/e8e90389.html","posts/60d9c4db.html","posts/20763.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
